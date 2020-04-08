8ebc7a12f30f09e886bf561e01e54b9957e0fe20
lijyhh <1285507636@qq.com>
2020-04-08 21:54:28 +0800

    Correct

--- git-acp.sh
+++ git-acp.sh
@@ -2,3 +2,11 @@
 git add .
 git commit -m $1
 git push -u origin master
+echo 'Study notes has finished.'
+
+cp ./Gvim_Linux/Linux学习笔记.md /c/Users/lijy花花/Desktop/Linux/LinuxCentos
+cd /c/Users/lijy花花/Desktop/Linux/LinuxCentos
+git add .
+git commit -m $1
+git push -u origin master
+echo 'Download notes has finished.'
